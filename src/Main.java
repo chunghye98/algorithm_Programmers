2055823a8debc05d42b47b707f0e5189baab2960
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-07 00:45:57 +0900

    add Replace

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 import lv1.*;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        StringSort s = new StringSort();
-        String[] strings1 = {"sun","bed","car"};
-        String[] strings2 = {"abce","abcd","cdx"};
-        int n1 = 1;
-        int n2 = 2;
-//        System.out.println(s.solution(strings1, n1));
-        System.out.println(Arrays.toString(s.solution(strings2, n2)));
+        Replace r = new Replace();
+        System.out.println(r.solution("one4seveneight"));
     }
 }
---
+++ src/lv1/Replace.java
@@ -0,0 +1,11 @@
+package lv1;
+
+public class Replace {
+    public int solution(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < 10; i++) {
+            s = s.replace(arr[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
