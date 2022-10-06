18ce151a7e50cd0879647225ce7ecca497e68c08
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-06 23:58:28 +0900

    add StringSort

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BruteForce b = new BruteForce();
-        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
-        System.out.println(b.solution(sizes));
+        StringSort s = new StringSort();
+        String[] strings1 = {"sun","bed","car"};
+        String[] strings2 = {"abce","abcd","cdx"};
+        int n1 = 1;
+        int n2 = 2;
+//        System.out.println(s.solution(strings1, n1));
+        System.out.println(Arrays.toString(s.solution(strings2, n2)));
     }
 }
